7dca7a07a54cfe360346b9102211f231d44e6a87
Eduardo David Paredes Vara <edpv@syaas.es>
2022-05-26 06:50:29 +0200

    Basic template

--- CVGenerator/settings.py
+++ CVGenerator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&0g9g-(30y@gul&x9w#2@vao*50#+m9up-v&7=$-u2i#8b739&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.104']
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.104', 'me.syaas.es']
 
 
 # Application definition
